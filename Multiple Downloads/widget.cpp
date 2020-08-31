7dd139d3ce63b5ba8c91a60b70f00e227a0d12a1
SaskarKhadka <68989438+SaskarKhadka@users.noreply.github.com>
2020-08-31 17:43:46 +0545

    Add files via upload

---
+++ Multiple Downloads/widget.cpp
@@ -0,0 +1,25 @@
+#include "widget.h"
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QFormLayout>
+#include <QProgressBar>
+#include <QLineEdit>
+#include <QApplication>
+#include <QFileDialog>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle("KU Loader 1.0.0");
+    setFixedSize(700,280);
+    initUi();
+}
+
+Widget::~Widget()
+{
+}
+
+
+
